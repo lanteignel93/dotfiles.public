84d9c9b0edf56b5a6f09fe85ea66c82b7cbb8c19
Laurent Lanteigne <laurent.lanteigne@gmail.com>
2026-09-25 20:49:55 -0500

    Sync from private dotfiles (451c837)

--- theme/build/spacecowboy/sckit.sh
+++ theme/build/spacecowboy/sckit.sh
@@ -5,3 +5,8 @@
   A_DIM=$'\e[38;2;122;104;82m'       # structure
   A_WARN=$'\e[38;2;240;192;144m'
   A_ERR=$'\e[38;2;224;122;95m'
+  # the sc logo's roles (dotclaude .claude/bin/lib/mkart.py), as r;g;b
+  SC_RGB_cursor='229;154;91' SC_RGB_fg='211;198;176' SC_RGB_fg2='169;154;133'
+  SC_RGB_bright='243;233;214' SC_RGB_dim='122;104;82' SC_RGB_border='138;106;82'
+  SC_RGB_lime='168;216;138' SC_RGB_mint='183;217;160' SC_RGB_peach='240;180;138'
+  SC_RGB_rose='224;122;95' SC_RGB_butter='240;192;144'
--- theme/build/voidrunner/sckit.sh
+++ theme/build/voidrunner/sckit.sh
@@ -5,3 +5,8 @@
   A_DIM=$'\e[38;2;88;88;88m'       # structure
   A_WARN=$'\e[38;2;255;255;186m'
   A_ERR=$'\e[38;2;222;166;160m'
+  # the sc logo's roles (dotclaude .claude/bin/lib/mkart.py), as r;g;b
+  SC_RGB_cursor='189;254;88' SC_RGB_fg='192;192;192' SC_RGB_fg2='158;158;158'
+  SC_RGB_bright='241;241;241' SC_RGB_dim='88;88;88' SC_RGB_border='88;88;88'
+  SC_RGB_lime='189;254;88' SC_RGB_mint='94;238;175' SC_RGB_peach='255;217;186'
+  SC_RGB_rose='222;166;160' SC_RGB_butter='255;255;186'
